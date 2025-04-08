8264a59947a3f7176cda699b5cc20231de5e10f5
rajansadhu3 <rajansadhu3@gmail.com>
2025-04-08 15:15:44 +0530

    base code

---
+++ main.js
@@ -0,0 +1,9 @@
+const heading = React.createElement(
+  "h1",
+  { id: "headingID" },
+  "Hello React text"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
